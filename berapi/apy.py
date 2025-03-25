f35c7ad7775a100f6a48065df0ecc01eedce0983
fachrulch <fachrul.fch@gmail.com>
2025-03-25 12:07:07 +0700

    add feature base url and headers

--- berapi/apy.py
+++ berapi/apy.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from urllib.parse import urljoin
 
 import curlify
 import requests
@@ -11,10 +12,21 @@ MAX_TIMEOUT = int(os.getenv("MAX_TIMEOUT", 3))
 MAX_RESPONSE_TIME = int(os.getenv("MAX_RESPONSE_TIME", 5))
 
 class berAPI(requests.Session):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, base_url=None, base_headers=None, *args, **kwargs):
         super(berAPI, self).__init__(*args, **kwargs)
         self.timeout = MAX_TIMEOUT
         self.hooks["response"].append(self._logging)
+        if base_url:
+            self.base_url = base_url
+        if base_headers:
+            self.headers.update(base_headers)
+
+    def url_handling(self, url):
+        print("URL Handling ", url, self.base_url)
+        if url.startswith("http"):
+            return url
+        else:
+            return urljoin(self.base_url, url)
 
     @staticmethod
     def _logging(response: requests.Response, *args, **kwargs):
@@ -48,16 +60,22 @@ class berAPI(requests.Session):
         return response
 
     def get(self, url, *args, **kwargs) -> Responder:
+        url = self.url_handling(url)
         return Responder(self.request("GET", url, *args, **kwargs))
 
     def post(self, url, *args, **kwargs):
+        url = self.url_handling(url)
+        print("URL aslinya ", url)
         return Responder(self.request("POST", url, *args, **kwargs))
 
     def put(self, url, *args, **kwargs):
+        url = self.url_handling(url)
         return Responder(self.request("PUT", url, *args, **kwargs))
 
     def patch(self, url, *args, **kwargs):
+        url = self.url_handling(url)
         return Responder(self.request("PATCH", url, *args, **kwargs))
 
     def delete(self, url, *args, **kwargs):
+        url = self.url_handling(url)
         return Responder(self.request("DELETE", url, *args, **kwargs))
--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,68 +1,70 @@
 import os
 
+import pytest
 from assertpy import assert_that
 from faker import Faker
 
 from berapi.apy import berAPI
 
 faker = Faker()
-a_user = {
-    "name": faker.name(),
-    "email": faker.safe_email(),
-    "gender": "male",
-    "status": "active"
-}
+
+
+@pytest.fixture()
+def api_client():
+    return berAPI(base_url='https://gorest.co.in/',
+                  base_headers={'Authorization': "Bearer " + os.getenv('API_TOKEN', 'xxx')})
+
 
 # Executed in sequence top-down
-
-def test_create_user():
-    header = {'Authorization': "Bearer " + os.getenv('API_TOKEN', 'ABC')}
-
-    response = (berAPI()
-                .post('https://gorest.co.in/public/v1/users', headers=header, json=a_user)
-                .assert_2xx()
-                .assert_response_time_less_than(seconds=3)
-                .parse_json()
-                )
-    a_user['id'] = response['data']['id']
-    assert_that(response['data']['id']).is_greater_than(1)
-    assert_that(response['data']['name']).is_equal_to(a_user['name'])
-    assert_that(response['data']['email']).is_equal_to(a_user['email'])
-    assert_that(response['data']['gender']).is_equal_to('male')
-
-
-def test_update_user():
-    print("==> Test 2", a_user)
-    payload = {
-        "status": "inactive",
-        "email": "updated+" + a_user['email']
+class TestCRUD:
+    a_user = {
+        "name": faker.name(),
+        "email": faker.safe_email(),
+        "gender": "male",
+        "status": "active"
     }
-    header = {'Authorization': "Bearer " + os.getenv('API_TOKEN', 'ABC')}
 
-    response = (berAPI()
-                .patch('https://gorest.co.in/public/v1/users/' + str(a_user['id']), headers=header, json=payload)
-                .assert_2xx()
-                .assert_response_time_less_than(seconds=3)
-                .parse_json()
-                )
+    def test_create_user(self, api_client):
+        response = (api_client
+                    .post('/public/v1/users', json=self.a_user)
+                    .assert_2xx()
+                    .assert_response_time_less_than(seconds=3)
+                    .parse_json()
+                    )
+        self.a_user['id'] = response['data']['id']
+        assert_that(response['data']['id']).is_greater_than(1)
+        assert_that(response['data']['name']).is_equal_to(self.a_user['name'])
+        assert_that(response['data']['email']).is_equal_to(self.a_user['email'])
+        assert_that(response['data']['gender']).is_equal_to('male')
 
-    assert_that(response['data']['email']).is_equal_to("updated+" + a_user['email'])
-    assert_that(response['data']['status']).is_equal_to("inactive")
+    def test_update_user(self, api_client):
+        print("==> Test 2", self.a_user)
+        payload = {
+            "status": "inactive",
+            "email": "updated+" + self.a_user['email']
+        }
+        response = (api_client
+                    .patch('/public/v1/users/' + str(self.a_user['id']), json=payload)
+                    .assert_2xx()
+                    .assert_response_time_less_than(seconds=3)
+                    .parse_json()
+                    )
 
+        assert_that(response['data']['email']).is_equal_to("updated+" + self.a_user['email'])
+        assert_that(response['data']['status']).is_equal_to("inactive")
 
-def test_delete_user():
-    header = {'Authorization': "Bearer " + os.getenv('API_TOKEN', 'ABC')}
-    (berAPI()
-     .delete('https://gorest.co.in/public/v1/users/' + str(a_user['id']), headers=header)
-     .assert_status_code(204)
-     .assert_response_time_less_than(seconds=3)
-     )
+    def test_delete_user(self, api_client):
+        (api_client
+         .delete('/public/v1/users/' + str(self.a_user['id']))
+         .assert_status_code(204)
+         .assert_response_time_less_than(seconds=3)
+         )
 
-    # re-delete
-    response = (berAPI()
-                .delete('https://gorest.co.in/public/v1/users/' + str(a_user['id']), headers=header)
-                .assert_status_code(404)
-                .assert_response_time_less_than(seconds=3)
-                .parse_json()
-                )
-    assert_that(response['data']['message']).is_equal_to("Resource not found")
+        # re-delete
+        response = (api_client
+                    .delete("/public/v1/users/{}".format(self.a_user['id']))
+                    .assert_status_code(404)
+                    .assert_response_time_less_than(seconds=3)
+                    .parse_json()
+                    )
+        assert_that(response['data']['message']).is_equal_to("Resource not found")
